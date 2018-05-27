24f263f09ad2dbaefaddcfe6270e8f2dac386e70
muriithiderro <muriithiderrick56@gmail.com>
2018-05-27 12:08:54 +0300

    Create main blueprint form classes
    Add form classes to handle various application functions such as updating post content.

---
+++ app/main/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField,TextAreaField,RadioField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
+from wtforms.fields.html5 import EmailField
+from wtforms.widgets import Input
+# from ..models import User
+
+class UpdateProfile(FlaskForm):
+	bio = TextAreaField("Tell us about you.", validators=[Required()])
+	submit = SubmitField('submit')
+
+
+class PostForm(FlaskForm):
+	body = TextAreaField("whats your pitch ?",validators=[Required()])
+	submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+	body = TextAreaField('',validators=[Required()])
+	submit = SubmitField()
+
+class SubscriptionForm(FlaskForm):
+	email = EmailField('Your Email Address',validators=[Required(),Email(message="I don't like your email.")])
+	submit = SubmitField('subscribe')
+
+class EditPost(FlaskForm):
+	body = TextAreaField("update post body.", validators=[Required()])
+	submit = SubmitField('submit')
