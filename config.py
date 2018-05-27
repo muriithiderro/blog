458dcde288278fc0b92021bd40d905ed164b1349
muriithiderro <muriithiderrick56@gmail.com>
2018-05-27 11:51:41 +0300

    Add Config file.
    Create a app config file for adding all the configurations for the application.

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+class Config:
+	SECRET_KEY = os.environ.get('SECRET_KEY')
+	# secret
+	SQLALCHEMY_TRACK_MODIFICATIONS=True
+	SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+		# "postgresql://postgres:postgres@localhost/bloggy"
+	UPLOADED_PHOTOS_DEST = "app/static/photos"
+
+	# email configurations
+	MAIL_SERVER = 'smtp.google.cpm'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get("MAIL.PASSWORD")
+
+
+	# simple mde configurations
+	SIMPLEMDE_JS_IIFE = True
+	SIMPLEMDE_USE_CDN = True
+	
+class ProdConfig(Config):
+	SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+	DEBUG=False
+
+class DevConfig(Config):
+	DEBUG = True
+
+class TestConfig(Config):
+	SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/test_bloggy"
+
+config_options = {
+	'development':DevConfig,
+	'production':ProdConfig,
+	'test': TestConfig,
+	'default':ProdConfig
+}
