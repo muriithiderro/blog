ecb0abca328ef8c01c2b2d98c556453729663430
muriithiderro <muriithiderrick56@gmail.com>
2018-05-27 12:13:44 +0300

    Create a manage.py file
    This describes the functionality of the app to incorporate startup functionality of the app,
    Interacting with the database and shell.

---
+++ manage.py
@@ -0,0 +1,33 @@
+from flask_migrate import Migrate,MigrateCommand
+from flask_script import Manager,Server
+
+#local imports
+from app import db,socketio
+from app import create_app
+
+from app.models import *
+from config import *
+
+app = create_app('development')
+
+# manager commands
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+# manager functionalities
+manager.add_command('db',MigrateCommand)
+manager.add_command('runserver',Server)
+
+# for shell functionalities.
+@manager.shell
+def make_shell_context():
+	return dict(app=app,db=db)
+
+# testing settings.
+@manager.command
+def test():
+	import unittest
+	tests = unittest.TestLoader().discover("tests")
+	unittest.TextTestRunner(verbosity=2).run(tests)
+if __name__ == '__main__':
+	manager.run()
